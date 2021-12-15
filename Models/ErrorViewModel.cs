493a229ba63d03365775c2a0363ec2b4c89ffe0e
DESKTOP-3UFFOGF\Igor-Strejevitch <brivia.istrejevitc.c@aes.com>
2021-12-15 12:47:20 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevelopersChallengeNIBO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
